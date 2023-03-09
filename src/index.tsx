b6e4dd68e6ab5978704a9944c2a02c20bedea84f
jwasp <nigushaa@yandex.ru>
2023-03-09 14:05:25 +0300

    deleted extra code

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
-import "./styles/global.scss";
+import "styles/global.scss";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (module.hot) {
+  module.hot.accept();
+}
